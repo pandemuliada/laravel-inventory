78dec40f0abee8496146b5ec8d77c9ede96bcf9c
pandemuliada <pandemuliada@gmail.com>
2020-05-24 09:48:31 +0800

    :sparkles: category index page

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-body">
+          <h2 class="title-2 mb-4">Category</h2>
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Created at</th>
+                <th scope="col">Updated at</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              @foreach ($categories as $category)
+
+              <tr>
+                <th scope="row">{{ $loop->iteration }}</th>
+                <td>{{ $category->name }}</td>
+                <td>{{ $category->created_at->format('m/d/Y') }}</td>
+                <td>{{ $category->updated_at->format('m/d/Y') }}</td>
+                <td>
+                  <button class="btn btn-sm btn-warning mr-2">Edit</button>
+                  <button class="btn btn-sm btn-outline-danger">Delete</button>
+                </td>
+              </tr>
+              @endforeach
+            </tbody>
+          </table>
+
+          {{ $categories->links() }}
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,5 +19,5 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('categories', 'CategoryController');
